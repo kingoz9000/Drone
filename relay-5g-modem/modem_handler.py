da050bf9c449119e93e43aba80735454abaf12d1
Muhsinscott112 <91264802+Muhsinscott112@users.noreply.github.com>
2025-03-03 11:39:00 +0100

    modem connection

--- relay-5g-modem/modem_handler.py
+++ relay-5g-modem/modem_handler.py
@@ -4,8 +4,9 @@ import socket
 class ModemHandler:
     """Handles the modem connection and sends AT commands"""
     def __init__(self): # sending AT commands to the modem
-        self.AT_COMMAND_PORT: int = 1 
-        self.AT_COMMAND_ADDRESS: str = ""
+        self.AT_MODEM_PORT: int = 1 # port for AT commands
+        self.AT_MODEM_ADRESS: str =  self.sendATCommand("AT+CGPADDR=1") # address for AT commands
+    
         self.AT_COMMAND_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         
     def sendATCommand(self, command: str) -> None # send AT command to modem
@@ -15,16 +16,23 @@ class ModemHandler:
         
     def setupConnection(self, apn = "internet"): # setup connection to modem
         # MUST be sent at least once everytime there is a firmware upgrade!
-        self.sendATCommand(f'AT+CGDCONT=1,"IP","{apn}"') # send AT command to modem
+        self.sendATCommand(f'AT+CGDCONT=1,"IP","{apn}"') # start PDP context
         
         # (Optional, debug only, AT commands) Activate PDP context, retrieve IP address and test with ping
-        self.sendATCommand("AT+CGACT=1,1") # send AT command to modem
+        self.sendATCommand("AT+CGACT=1,1") # Activate PDP context
         return self.sendATCommand("AT+CGPADDR=1") # Get IP address from modem
     
     def getSignalStrength(self): # (Optional, debug only, AT commands) Retrieve signal strength 
         return self.sendATCommand("AT+CSQ")
-                  
-        
+    
+    def checkStatus(self):
+        "check if modem is responsive, by sending AT command"
+        response = self.sendATCommand("AT") 
+        if response == "OK":
+            return True
+        else:   
+            return False
+    
 if __name__ == "__main__":
     modem = ModemHandler()
     modem.setupConnection()
