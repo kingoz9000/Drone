a69c02615193e51dff38f7dd3195b96d6d28914a
Muhsinscott112 <91264802+Muhsinscott112@users.noreply.github.com>
2025-03-03 16:07:40 +0100

    modem

--- relay-5g-modem/modem_handler.py
+++ relay-5g-modem/modem_handler.py
@@ -6,13 +6,14 @@ class ModemHandler:
     def __init__(self): 
         # sending AT commands to the modem
         self.AT_MODEM_PORT: int = 5000 # port for AT commands
-        self.AT_MODEM_ADDRESS: str =  "" # address for AT commands
+        self.BAUDRATE: int = 115200 # baudrate for AT commands
+        self.AT_CARRIER_ADDRESS: str =  "" # carrier IP address
         self.AT_COMMAND_SOCKET = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.AT_SOCKET.settimeout(30) # set timeout for socket to 30 seconds
         
     def sendATCommand(self, command: str) -> None: # send AT command to modem
         try:
-            self.AT_COMMAND_SOCKET.sendto(command.encode('utf-8'), (self.AT_COMMAND_ADDRESS, self.AT_COMMAND_PORT)) # send AT command to modem
+            self.AT_COMMAND_SOCKET.sendto(command.encode('utf-8'), (self.AT_CARRIER_ADDRESS, self.AT_MODEM_PORT)) # send AT command to modem
             response = self.AT_COMMAND_SOCKET.recv(1024) # recieve response from modem
             return response.decode() # return response from modem
         except socket.timeout:
@@ -33,7 +34,12 @@ class ModemHandler:
             print(f"An error occured while sending AT command: {e}")
     
     def getSignalStrength(self): # (Optional, debug only, AT commands) Retrieve signal strength 
-        return self.sendATCommand("AT+CSQ")
+        try:
+            return self.sendATCommand("AT+CSQ")
+        except socket.timeout:
+            print("Socket timed out")
+        except Exception as e:
+            print(f"An error occured while sending AT command: {e}")   
     
     def checkStatus(self) -> bool: 
         "check if modem is responsive, by sending AT command"
