e8b0e3014b99e42cee19cc2f1de52d70b210b8ae
zaddmc <davidbk927@gmail.com>
2025-03-04 11:46:53 +0100

    Fuck ipv6 testing, it will never succed

--- ipv6_test/client.py
+++
@@ -1,9 +0,0 @@
-import socket
-
-sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM, 0)
-sock.connect(("fd9f:87a9:7276::282/128", 4565, 0, 0))
-
-sock.send(bytes("Fish", "utf-8"))
-data = sock.recv(1024)
-print(data.decode("utf-8"))
-sock.close()
