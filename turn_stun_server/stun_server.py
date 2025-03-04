53663d8a9237319ce339e95257ac3e73c8b6d397
smmarkvard <smmarkvard@gmail.com>
2025-03-04 15:31:07 +0100

    lort

--- turn_stun_server/stun_server.py
+++ turn_stun_server/stun_server.py
@@ -10,6 +10,11 @@ class StunServer:
         self.server_socket.bind((self.SERVER_IP, self.SERVER_PORT))
         print(f"Server listening on {self.SERVER_IP}:{self.SERVER_PORT}")
 
+    def get_client_id(self, addr):
+        for k, v in self.clients.items():
+            if v[0] == addr:
+                return k
+        return None
 
     def exchange(self):
         while True:
@@ -19,26 +24,26 @@ class StunServer:
             if message.startswith("REGISTER"):
 
                 client_id = len(self.clients) 
-                self.clients[client_id] = 
+                self.clients[client_id] = [addr, None]
                 self.server_socket.sendto(f"REGISTERED {self.clients}".encode(), addr)
                 print(f"Client {client_id} registered from {addr}")
 
             elif message.startswith("REQUEST"):
                 _, target_id = message.split()
                 target_id = int(target_id)
+                current_client_id = self.get_client_id(addr)
 
                 if target_id in self.clients:
-                    for key, value in self.clients.items():
-                        if addr == value[0]:
-                            self.clients[key][1] = target_id
-                    target_addr = self.clients[target_id]
-                    requester_id = [k for k, v in self.clients.items() if v == addr][0]
+                    self.clients[current_client_id][1] = target_id
+                    target_addr = self.clients[target_id][0]
 
-                    # Send both clients each other's public IP and port
-                    self.server_socket.sendto(f"SERVER CONNECT {target_addr[0]} {target_addr[1]}".encode(), addr)
-                    self.server_socket.sendto(f"SERVER CONNECT {addr[0]} {addr[1]}".encode(), target_addr)
-                    print(f"Exchanged details between Client {requester_id} and Client {target_id}")
-
+                    if self.clients[target_id][1] == current_client_id:
+                        # Send both clients each other's public IP and port
+                        self.server_socket.sendto(f"PEER {target_addr[0]} {target_addr[1]}".encode(), addr)
+                        self.server_socket.sendto(f"PEER {addr[0]} {addr[1]}".encode(), target_addr)
+                        print(f"Exchanged details between Client {current_client_id} and Client {target_id}")
+                    else:
+                        print(f"Client {current_client_id} requested Client {target_id}, but target not set reciprocally.")
                 else:
                     self.server_socket.sendto("NOT_FOUND".encode(), addr)
 
