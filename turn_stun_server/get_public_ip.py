820e1b66f9519ac23092e2bc5d55281bfc7ced9d
Mathias Sørensen <mkrs24@student.aau.dk>
2025-03-05 10:18:16 +0100

    Cleaned double folders

--- turn_stun_server/get_public_ip.py
+++
@@ -1,8 +0,0 @@
-import stun
-
-nat_type, external_ip, external_port = stun.get_ip_info(stun_host="130.225.37.157", stun_port=3478)
-
-print(f"Detected NAT Type: {nat_type}")
-print(f"Public IP: {external_ip}")
-print(f"Public Port: {external_port}")
-
