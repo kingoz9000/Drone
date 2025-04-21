b44fbfc9e6836ca067f8b29c62c8379507eab1ea
Mathias Sørensen <mkrs24@student.aau.dk>
2025-04-21 16:06:36 +0200

    Need inheritance

--- GUI/stun/stun_client.py
+++ GUI/stun/stun_client.py
@@ -4,7 +4,6 @@ import time
 
 
 class StunClient:
-
     def __init__(self):
         self.stun_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
         self.stun_socket.bind(("", 0))
@@ -12,22 +11,27 @@ class StunClient:
         self.client_id = None
         self.peer_addr = None
 
-        self.SERVER_ADDR = ("130.225.37.157", 12345)
+        self.STUN_SERVER_ADDR = ("130.225.37.157", 12345)
 
         self.HOLE_PUNCH_TRIES = 5
         self.hole_punched = False
 
         self.running = True
         self.relay = False
+        self.stats_refresh_rate = 0.5  # seconds
 
-        self.command_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.command_addr = ("192.168.10.1", 8889)
+        # Communication with drone
+        self.drone_command_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.drone_command_addr = ("192.168.10.1", 8889)
 
-        self.video_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-        self.video_socket.bind(("0.0.0.0", 11111))
+        self.drone_video_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.drone_video_socket.bind(("0.0.0.0", 11111))
+
+        self.state = None
+        self.response = None
 
     def register(self):
-        self.stun_socket.sendto(b"REGISTER", self.SERVER_ADDR)
+        self.stun_socket.sendto(b"REGISTER", self.STUN_SERVER_ADDR)
         response, _ = self.stun_socket.recvfrom(4096)
         self.client_id = response.decode().split()[1]
         print(f"Registered with ID: {self.client_id}")
@@ -36,10 +40,17 @@ class StunClient:
         if self.peer_addr:
             return self.peer_addr
 
+    @staticmethod
+    def run_in_thread(func, *args) -> threading.Thread:
+        """General worker function to run a function in a thread"""
+        thread = threading.Thread(target=func, args=args, daemon=True)
+        thread.start()
+        return thread
+
     def request_peer(self):
-        self.stun_socket.sendto(b"CHECK", self.SERVER_ADDR)
+        self.stun_socket.sendto(b"CHECK", self.STUN_SERVER_ADDR)
         peer_id = input("Enter peer ID: ")
-        self.stun_socket.sendto(f"REQUEST {peer_id}".encode(), self.SERVER_ADDR)
+        self.stun_socket.sendto(f"REQUEST {peer_id}".encode(), self.STUN_SERVER_ADDR)
 
     def start_connection_listener(self):
         self.listen_thread = threading.Thread(target=self.listen, daemon=True)
@@ -50,8 +61,27 @@ class StunClient:
             data = self.stun_socket.recv(4096)
 
             if not self.relay and self.hole_punched:
-                self.stun_socket.sendto(data, ("127.0.0.1", 27463))
-                continue
+                # Loopback for the operator
+                flag = data[0]
+
+                # Check if the first byte is 0 or 1
+                # If 0 send to loopback (videofeed)
+                if flag == 0:
+                    data = data[1:]
+                    self.stun_socket.sendto(data, ("127.0.0.1", 27463))
+                    continue
+
+                # Response
+                elif flag == 1:
+                    # Skal sendes til TKinter
+                    self.state = data[1:]
+                    continue
+
+                # State
+                elif flag == 2:
+                    self.response = data[1:]
+                    continue
+
             message = data.decode()
 
             if message.startswith("SERVER"):
@@ -62,7 +92,7 @@ class StunClient:
                     self.hole_punch()
 
                 if message.split()[1] == "HEARTBEAT":
-                    self.stun_socket.sendto(b"ALIVE", self.SERVER_ADDR)
+                    self.stun_socket.sendto(b"ALIVE", self.STUN_SERVER_ADDR)
 
                 if message.split()[1] == "DISCONNECT":
                     print("Server disconnected due to other client disconnection")
@@ -75,7 +105,7 @@ class StunClient:
             if message.startswith("HOLE") and not self.hole_punched:
                 self.hole_punched = True
                 print("Hole punched!")
-                self.stun_socket.sendto(b"HOLE PUNCHED", self.SERVER_ADDR)
+                self.stun_socket.sendto(b"HOLE PUNCHED", self.STUN_SERVER_ADDR)
 
             if message.startswith("PEER"):
                 # intended for the relay
@@ -92,18 +122,29 @@ class StunClient:
         self.stun_socket.sendto(command.encode(), self.peer_addr)
 
     def send_command_to_drone(self, command, take_response=False):
-        self.command_socket.sendto(bytes(command, "utf-8"), self.command_addr)
+        self.drone_command_socket.sendto(
+            bytes(command, "utf-8"), self.drone_command_addr
+        )
         if take_response:
-            self.command_socket.settimeout(0.5)
+            self.drone_command_socket.settimeout(0.5)
             try:
-                response = self.command_socket.recv(1024).decode()
-                self.send_response_to_operator(response)
+                response = self.drone_command_socket.recv(1024).decode()
+                self.send_data_to_operator(response, prefix=1)
             except socket.timeout:
                 print(f"Command '{command}': No response received within 0.5 seconds")
 
-    def send_response_to_operator(self, response):
-        
-        pass
+    def send_data_to_operator(self, data, prefix=0):
+        shifted = bytearray([prefix]) + data
+        client.stun_socket.sendto(shifted, client.peer_addr)
+
+    def state_socket_handler(self):
+        state_addr = ("0.0.0.0", 8890)
+        self.state_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.state_socket.bind(state_addr)
+        while self.running:
+            state = self.drone_video_socket.recv(4096)
+            self.send_data_to_operator(state, prefix=2)
+            time.sleep(self.stats_refresh_rate)
 
     def main(self):
         self.register()
@@ -114,10 +155,12 @@ class StunClient:
 if __name__ == "__main__":
     client = StunClient()
     client.relay = True
-    client.command_socket.bind(("0.0.0.0", 9000))
+    # Response from drone to the relay but not to the operator
+    client.drone_command_socket.bind(("0.0.0.0", 9000))
     client.main()
+    client.run_in_thread(client.state_socket_handler)
 
     while client.running:
         if client.hole_punched:
-            msg = client.video_socket.recv(4096)
-            client.stun_socket.sendto(msg, client.peer_addr)
+            msg = client.drone_video_socket.recv(4096)
+            client.send_data_to_operator(msg)
