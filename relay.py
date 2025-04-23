5ed89207e6427011e978c18b426a44252dbc48dc
Rasmus <rasser1357@gmail.com>
2025-04-23 11:14:56 +0200

    correct file

--- relay.py
+++ relay.py
@@ -7,7 +7,13 @@ client.drone_command_socket.bind(("0.0.0.0", 9000))
 client.main()
 client.run_in_thread(client.state_socket_handler)
 
+seq_num = 0
+
 while client.running:
     if client.hole_punched:
+        seq_byte = seq_num.to_bytes(2, 'big')
+
         msg = client.drone_video_socket.recv(4096)
-        client.send_data_to_operator(msg)
+        client.send_data_to_operator(seq_byte + msg)
+        print(f"From relay: {seq_num}")
+        seq_num = (seq_num + 1) % 65536
--- stun/relay_stun_client.py
+++ stun/relay_stun_client.py
@@ -90,23 +90,3 @@ class RelayStunClient(StunClient):
 
             # This is command forwarding (from operator to drone)
             self.send_command_to_drone(message, take_response=False)
-
-
-if __name__ == "__main__":
-    client = RelayStunClient()
-    client.relay = True
-    # Response from drone to the relay but not to the operator
-    client.drone_command_socket.bind(("0.0.0.0", 9000))
-    client.main()
-    client.run_in_thread(client.state_socket_handler)
-
-    seq_num = 0
-
-    while client.running:
-        if client.hole_punched:
-            seq_byte = seq_num.to_bytes(2, 'big')
-
-            msg = client.drone_video_socket.recv(4096)
-            client.send_data_to_operator(seq_byte + msg)
-            print(f"From relay: {seq_num}")
-            seq_num = (seq_num + 1) % 65536
